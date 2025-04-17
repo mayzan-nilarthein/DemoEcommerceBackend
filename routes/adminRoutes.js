890eb197acce6ec84b7fa68c79a18f2154194f0d
mayzan-nilarthein <mayzan.nilarthein@gmail.com>
2025-04-17 13:48:25 +0630

    feat(admim) : added two otp-related routes to adminRoutes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,9 +3,13 @@ const router = express.Router();
 const {
   registerAdmin,
   loginAdmin,
+  sendOTP,
+  verifyOTP,
 } = require("../controllers/adminControllers");
 
 router.post("/register", registerAdmin);
 router.post("/login", loginAdmin);
+router.post("/send-otp", sendOTP);
+router.post("/verify-otp", verifyOTP);
 
 module.exports = router;
